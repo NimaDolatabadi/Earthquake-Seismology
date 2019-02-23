c26a84e2ac3a7b89a91d22de7671106e5ca22629
Nima Dolatabadi <32169409+NimaDolatabadi@users.noreply.github.com>
2019-02-23 14:57:11 +0330

    Update seisan-config-full.sh

--- seisan-config-full.sh
+++ seisan-config-full.sh
@@ -6,17 +6,17 @@
 ################### Put This Script In your SEISAN Home Directory ########################
 
 ###### First do 'sudo su', due to your distribution ############
-
+sudo su
 apt-get install gfortran libhdf5-serial-dev libfftw3-dev gfortran gcc-4.8 libpcre3-dev libgtk2.0-dev subversion ghostscript build-essential cmake libnetcdf-dev libgdal1-dev
 add-apt-repository ppa:ubuntu-toolchain-r/test && apt-get update
 update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
 pwd=`pwd`
 NMXHOME="/home/nima/src/seismo/LIB/nmx/"
 cd COM/
-sed -i "16s export SEISAN_TOP=$pwd/" SEISAN.bash
+sed -i "16i export SEISAN_TOP=$pwd" SEISAN.bash
 source SEISAN.bash
 cd ../PRO
-sed -i "120s INSTALL_PRO_PATH = $pwd/pro" MakeFile
+sed -i "120i INSTALL_PRO_PATH = $pwd/pro" Makefile
 make all
 make install
 cd ../LIB/
