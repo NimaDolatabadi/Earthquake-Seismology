b1d6ebff427a4444df1ea09b9b16bcc4dfee0a6f
Nima Dolatabadi <32169409+NimaDolatabadi@users.noreply.github.com>
2019-11-05 14:45:08 +0330

    Add files via upload

---
+++ GMT4-installation.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+##### This Script Has Been Written By N.Dolatabadi ##########################################
+
+#### If you were in need, contact me ------> syn_org939@rocketmail.com ######################
+#### You can have a look at my github too ------> https://github.com/NimaDolatabadi/Earthquake-Seismology ######
+
+###### GMT4 Installation ######
+
+#### Uncomment if you're in need of this version of GMT ########
+#### SAC, Works with GMT4 #######
+sudo su
+wget -c --retry-connrefused --tries=0 --timeout=5 http://gmt.mirror.ac.za/legacy/gmt-4.5.16-src.tar.bz2
+mkdir /opt/gmt-4.5.16
+tar -xf gmt-4.5.16-src.tar.bz2
+cd gmt-4.5.16
+pwd=pwd
+wget -c --retry-connrefused --tries=0 --timeout=5 http://gmt.mirror.ac.za/legacy/gshhs-2.2.0.tar.bz2
+tar -xf gshhs-2.2.0.tar.bz2
+./configure --prefix= /home/nima/src/GMT-4.5.16 --with-gshhg-dir= $pwd/gshhs-2.2.0
+make && make install && make install-gmt && make install-data && make spotless && make install-man && make install-doc && make examples
+
+echo '# GMT-4.5.16' >> ~/.bashrc
+echo "export GMT4HOME=/opt/GMT-4.5.16" >> ~/.bashrc
+echo "export PATH=${GMT4HOME}/bin:${PATH}" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${GMT4HOME}/lib" >> ~/.bashrc
+echo "export C_INCLUDE_PATH=$C_INCLUDE_PATH:${GMT4HOME}/include" >> ~/.bashrc
+
+source ~/.bashrc
