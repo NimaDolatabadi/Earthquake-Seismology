a1d6227abbfe824012faea261f56a93411859ab6
Nima Dolatabadi <32169409+NimaDolatabadi@users.noreply.github.com>
2023-04-08 17:07:03 +0330

    Update ISC-Catalogue.sh

--- ISC-Catalogue.sh
+++ ISC-Catalogue.sh
@@ -12,7 +12,7 @@ sudo apt install curl links
 # Let's Define some Variables in which you can fill it
 # to your needs.
 req="COMPREHENSIVE"	# request Type: ISC Bulletin(COMPREHENSIVE) && Reviewed ISC Bulletin(REVIEWED)
-out="CATCSV"		# output format: CSV(CATCSV) && QuakeML(CATQuakeML)
+out="CATCSV"		# output format: CSV(CATCSV)
 sy="2000"		# start year
 sm="01"	 		# start month
 sd="01"			# start day
@@ -20,14 +20,12 @@ st="00%3A00%3A00"	# start time(HH%3AMM%3ASS)
 ey="2020"		# end year
 em="01"			# end month
 ed="01"			# end day
-et="00%3A00%3A00"	# end time(HH%3AMM%3ASS)
+et="23%3A59%3A59"	# end time(HH%3AMM%3ASS)
 sesh="RECT"		# search shape: Rectangle(RECT) & Circle(CIRC) & Global(GLOBAL) & Flinn-Engdahl(FE)
 clat="" 		# central latitude
 clon=""			# central longitude
 rad=""			# search radius
 mdu=""			# search radius Unit (deg / km)
-srn=""			# seismic region number (1 to 50)
-grn=""			# geographic region number (1 to 757)
 minlat="33"		# minimum latitude
 maxlat="36"		# maximum latitude
 minlon="45"		# minimum longitude
@@ -37,7 +35,7 @@ maxmag="10"		# maximum magnitude
 magtype="Any"		# magnitude type(MB/MS/MW/MD/ML)
 rma="Any"		# magnitude author (check https://github.com/NimaDolatabadi/Earthquake-Seismology/blob/EE/ISC-Agencies.csv)
 
-# Now it's time to get the catalogue from WWW.ISC.AC.UK
+# Now it's time to get the catalogue from HTTP://WWW.ISC.AC.UK/ISCBULLETIN/SEARCH/CATALOGUE/
 
 curl -G --cookie cjar --cookie-jar cjar \
 	-d "out_format=$out" \
@@ -50,8 +48,6 @@ curl -G --cookie cjar --cookie-jar cjar \
 	-d "ctr_lon=$clon" \
 	-d "radius=$rad" \
 	-d "max_dist_units=$mdu" \
-	-d "srn=$srn" \
-	-d "grn=$grn" \
 	-d "start_year=$sy" \
 	-d "start_month=$sm" \
 	-d "start_day=$sd" \
@@ -71,11 +67,11 @@ curl -G --cookie cjar --cookie-jar cjar \
 # Just adding some modificatoin so thats it would be nice in view and usage
 links -dump Result.html > Result.txt
 end_no=`cat Result.txt | grep -n STOP | awk -F ":" '{print $1-1}'`
-cat Result.txt | sed -e "$end_no,\$d" | sed -e '1,28d' | awk -F " " '{for(i=1;i<=NF;i++) printf "%s ",$i; print ""}' > Result-Regular.txt
-#cat Result-Regular.txt | awk -F "]" '{print $2}' | awk -F "[/:]" '{print $1,$2,$3,$4,$5}' | sort > Final-IRSC-Catalogue.txt
-#sed -i '1i Year Mon Day Hour Min Sec Lat Lon Depth MN' Final-IRSC-Catalogue.txt
-#sed -i 's/ /,/g' Final-IRSC-Catalogue.txt
-#rm -r Result.html Result.txt Result-Regular.txt cjar
-#mv Final-IRSC-Catalogue.txt IRSC.$sy$sm$sd.$ey$em$ed.csv
+cat Result.txt | sed -e "$end_no,\$d" | sed -e '1,29d' | awk -F " " '{for(i=1;i<=NF;i++) printf "%s ",$i; print ""}' > Result-Regular.txt
+cat Result-Regular.txt | sed 's/ //g' | sed 's/-/,/g' | sed 's/:/,/g' | awk -F "," '{print $1,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$15,$16}' > Final-ISC-Catalogue.txt
+sed -i '1i EventID Author Year Mon Day Hour Min Sec Lat Lon Depth MagType MagValue' Final-ISC-Catalogue.txt
+sed -i 's/ /,/g' Final-ISC-Catalogue.txt
+rm -r Result.html Result.txt Result-Regular.txt cjar
+mv Final-ISC-Catalogue.txt ISC.$sy$sm$sd.$ey$em$ed.csv
 
-echo 'WELL DONE'
+echo 'WOW You are a Materpiece'
